1930e81626c9871cf6523944b80b8e3d77f49da9
[dyad] <git@dyad.sh>
2025-11-12 22:45:38 -0300

    [dyad] wrote 1 file(s)

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -146,7 +146,6 @@ const Inscricao = () => {
                   />
                 </div>
                 
-                {/* Restaurado para grid md:grid-cols-3 para que os 3 campos fiquem lado a lado */}
                 <div className="grid md:grid-cols-3 gap-4"> 
                   <FormField
                     control={form.control}
@@ -164,13 +163,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="feminino" id="feminino" />
                               </FormControl>
-                              <FormLabel htmlFor="feminino" className="font-normal cursor-pointer">Feminino</FormLabel>
+                              <FormLabel htmlFor="feminino" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Feminino</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="masculino" id="masculino" />
                               </FormControl>
-                              <FormLabel htmlFor="masculino" className="font-normal cursor-pointer">Masculino</FormLabel>
+                              <FormLabel htmlFor="masculino" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Masculino</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
@@ -195,13 +194,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="sim" id="programas-sim" />
                               </FormControl>
-                              <FormLabel htmlFor="programas-sim" className="font-normal cursor-pointer">Sim</FormLabel>
+                              <FormLabel htmlFor="programas-sim" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Sim</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="nao" id="programas-nao" />
                               </FormControl>
-                              <FormLabel htmlFor="programas-nao" className="font-normal cursor-pointer">Não</FormLabel>
+                              <FormLabel htmlFor="programas-nao" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Não</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
@@ -226,13 +225,13 @@ const Inscricao = () => {
                               <FormControl>
                                 <RadioGroupItem value="sim" id="aceita-sim" />
                               </FormControl>
-                              <FormLabel htmlFor="aceita-sim" className="font-normal cursor-pointer">Sim</FormLabel>
+                              <FormLabel htmlFor="aceita-sim" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Sim</FormLabel>
                             </FormItem>
                             <FormItem className="flex items-center space-x-2">
                               <FormControl>
                                 <RadioGroupItem value="nao" id="aceita-nao" />
                               </FormControl>
-                              <FormLabel htmlFor="aceita-nao" className="font-normal cursor-pointer">Não</FormLabel>
+                              <FormLabel htmlFor="aceita-nao" className="font-normal cursor-pointer whitespace-nowrap flex-shrink-0">Não</FormLabel>
                             </FormItem>
                           </RadioGroup>
                         </FormControl>
